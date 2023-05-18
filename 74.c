35d84e0d35bddabf175b2504a6cdb9556dd8c447
samikshagla <131234048+samikshagla@users.noreply.github.com>
2023-05-19 02:09:30 +0530

    Create 74.c

---
+++ 74.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100
+
+int main() {
+    int matrix[MAX_SIZE][MAX_SIZE];
+    int size, i, j, sum = 0;
+
+    printf("Enter the size of the matrix: ");
+    scanf("%d", &size);
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Matrix:\n");
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+  
+    for (i = 0; i < size; i++) {
+        for (j = i; j < size; j++) {
+            sum += matrix[i][j];
+        }
+    }
+
+    printf("Sum of upper triangular matrix: %d\n", sum);
+
+    return 0;
+}
