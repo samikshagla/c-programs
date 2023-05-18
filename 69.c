40b3a07e3dd83438403503834021e025652d33c9
samikshagla <131234048+samikshagla@users.noreply.github.com>
2023-05-19 02:02:53 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define MAX_SIZE 100
+
+int main() {
+    int matrix[MAX_SIZE][MAX_SIZE];
+    int size, i, j, sum = 0;
+
+    printf("Enter the size of the matrix: ");
+    scanf("%d", &size);
+
+    printf("Enter the elements of the matrix:\n");
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    printf("Matrix:\n");
+    for (i = 0; i < size; i++) {
+        for (j = 0; j < size; j++) {
+            printf("%d ", matrix[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("Minor diagonal elements: ");
+    for (i = 0; i < size; i++) {
+        sum += matrix[i][size - i - 1];
+    }
+
+    printf("\nSum of minor diagonal elements: %d\n", sum);
+
+    return 0;
+}
