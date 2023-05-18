a4aaf7dc1cb78b86abd2bc21888ba97adc2ffa90
samikshagla <131234048+samikshagla@users.noreply.github.com>
2023-05-19 00:39:11 +0530

    Create 25.c

---
+++ 25.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+int main()
+{
+    int num1, num2, num3, max;
+    printf("Enter three numbers: ");
+    scanf("%d%d%d", &num1, &num2, &num3);
+    if((num1 > num2) && (num1 > num3))
+    {
+        max = num1;
+    }
+    else if((num2 > num1) && (num2 > num3))
+    {
+        max = num2;
+    }
+    else if((num3 > num1) && (num3 > num2))
+    {
+        max = num3;
+    }
+    printf("Maximum among all three numbers = %d", max);
+    return 0;
+}
