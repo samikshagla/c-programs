3b720085855f6022f601d11c4b168cfaa717e010
samikshagla <131234048+samikshagla@users.noreply.github.com>
2023-05-21 11:11:15 +0530

    Create 95.c

---
+++ 95.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int isPalindrome(int num) {
+    int originalNum = num;
+    int reversedNum = 0;
+
+    while (num != 0) {
+        int remainder = num % 10;
+        reversedNum = reversedNum * 10 + remainder;
+        num /= 10;
+    }
+
+    return originalNum == reversedNum;
+}
+
+int main() {
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if (isPalindrome(num)) {
+        printf("%d is a palindrome.\n", num);
+    } else {
+        printf("%d is not a palindrome.\n", num);
+    }
+
+    return 0;
+}
