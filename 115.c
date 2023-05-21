d6933a7d72fb0884c964f03095c51c37d509911a
samikshagla <131234048+samikshagla@users.noreply.github.com>
2023-05-21 11:47:02 +0530

    Create 115.c

---
+++ 115.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#include <string.h>
+
+
+bool isPalindrome(const char *str) {
+    int length = strlen(str);
+    int start = 0;
+    int end = length - 1;
+
+    while (start < end) {
+        if (str[start] != str[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+
+    return true;
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%[^\n]", str);
+
+    if (isPalindrome(str)) {
+        printf("The string is a palindrome.\n");
+    } else {
+        printf("The string is not a palindrome.\n");
+    }
+
+    return 0;
+}
